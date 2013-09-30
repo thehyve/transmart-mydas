6ef68abc7bdeec6ffe92477645bc777b5b0b2d30
Riza Nugraha <rnugraha@gmail.com>
2013-09-30 14:59:26 +0200

    GWAS-71 Visualization for Summary Level MAF

--- grails-app/services/transmart/mydas/VcfService.groovy
+++ grails-app/services/transmart/mydas/VcfService.groovy
@@ -7,6 +7,7 @@ import uk.ac.ebi.mydas.model.DasFeature
 import uk.ac.ebi.mydas.model.DasFeatureOrientation
 import uk.ac.ebi.mydas.model.DasPhase
 import uk.ac.ebi.mydas.model.DasType
+import uk.ac.ebi.mydas.model.Range
 
 /**
  *
@@ -25,7 +26,9 @@ class VcfService {
     List<DasAnnotatedSegment> getVCFFeatures(Long resultInstanceId,
                                              Collection<String> segmentIds = [],
                                              Integer maxbins = null,
-                                             uk.ac.ebi.mydas.model.Range range = null ) throws UnimplementedFeatureException {
+                                             Range range = null ) throws UnimplementedFeatureException {
+
+        // TODO to retrieve from backend
 
         // -----------------------------------------------
         // code below is only intended to serve dummy data
@@ -61,6 +64,56 @@ class VcfService {
         ]
 
         [new DasAnnotatedSegment('22' , 30109479 , 30352561 , 'dummy.version', 'label for segment 22', dummyFeatures)]
+    }
+
+    /**
+     * Retrieve summary level of Minor Alele Frequency
+     * @param segmentIds
+     * @param maxbins
+     * @param range
+     * @return
+     */
+    List<DasAnnotatedSegment> getSummaryMAF(Collection<String> segmentIds = [],
+                                            Integer maxbins = null,
+                                            Range range = null) {
+
+        // TODO to retrieve from backend
+
+        // -----------------------------------------------
+        // code below is only intended to serve dummy data
+        // -----------------------------------------------
+
+        def dummyFeatures = [
+                new DasFeature("summary-maf-1","summary-maf-1",new DasType("smaf", "", "", ""),vcfMethod,30188040,30188040,0.7,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-2","summary-maf-2",new DasType("smaf", "", "", ""),vcfMethod,30189100,30189100,0.6,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-3","summary-maf-3",new DasType("smaf", "", "", ""),vcfMethod,30189120,30189120,0.1,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-4","summary-maf-4",new DasType("smaf", "", "", ""),vcfMethod,30189150,30189150,0.7,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-5","summary-maf-5",new DasType("smaf", "", "", ""),vcfMethod,30189188,30189188,0.2,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-6","summary-maf-6",new DasType("smaf", "", "", ""),vcfMethod,30189112,30189112,0.3,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-7","summary-maf-7",new DasType("smaf", "", "", ""),vcfMethod,30189134,30189134,0.24,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-8","summary-maf-8",new DasType("smaf", "", "", ""),vcfMethod,30189114,30189114,0.7,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-9","summary-maf-9",new DasType("smaf", "", "", ""),vcfMethod,30189199,30189199,0.4,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-10","summary-maf-10",new DasType("smaf", "", "", ""),vcfMethod,30189145,30189145,0.2,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-11","summary-maf-11",new DasType("smaf", "", "", ""),vcfMethod,30189142,30189142,0.3,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-12","summary-maf-12",new DasType("smaf", "", "", ""),vcfMethod,30189176,30189176,0.2,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-13","summary-maf-13",new DasType("smaf", "", "", ""),vcfMethod,30189182,30189182,0.3,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-14","summary-maf-14",new DasType("smaf", "", "", ""),vcfMethod,30189200,30189200,0.6,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-15","summary-maf-15",new DasType("smaf", "", "", ""),vcfMethod,30189243,30189243,0.9,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-16","summary-maf-16",new DasType("smaf", "", "", ""),vcfMethod,30189287,30189287,0.2,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-17","summary-maf-17",new DasType("smaf", "", "", ""),vcfMethod,30189278,30189278,0.2,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-18","summary-maf-18",new DasType("smaf", "", "", ""),vcfMethod,30189213,30189213,0.1,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-19","summary-maf-19",new DasType("smaf", "", "", ""),vcfMethod,30189254,30189254,0.7,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-20","summary-maf-20",new DasType("smaf", "", "", ""),vcfMethod,30189253,30189253,0.2,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-21","summary-maf-21",new DasType("smaf", "", "", ""),vcfMethod,30189224,30189224,0.5,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-22","summary-maf-22",new DasType("smaf", "", "", ""),vcfMethod,30189210,30189210,0.5,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-23","summary-maf-23",new DasType("smaf", "", "", ""),vcfMethod,30189299,30189299,0.3,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-24","summary-maf-24",new DasType("smaf", "", "", ""),vcfMethod,30189265,30189265,0.9,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+                new DasFeature("summary-maf-25","summary-maf-25",new DasType("smaf", "", "", ""),vcfMethod,30189262,30189262,0.8,DasFeatureOrientation.ORIENTATION_ANTISENSE_STRAND,DasPhase.PHASE_NOT_APPLICABLE,[],[:],[],[],[]),
+
+        ]
+
+        [new DasAnnotatedSegment('22' , 30109479 , 30352561 , 'dummy.version', 'label for segment 22', dummyFeatures)]
+
     }
 
 }
